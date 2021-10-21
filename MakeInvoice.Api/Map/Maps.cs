56218d18d2b32516639686f074a4a44650fc49cd
Alexey Bubley <alexey.bubley@discovercars.com>
2021-10-21 09:23:02 +0300

    Add automapper

---
+++ MakeInvoice.Api/Map/Maps.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MakeInoice.Common.ViewModels;
+using MakeInvoice.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeInvoice.Api.Map
+{
+    public class Maps: Profile
+    {
+        public Maps()
+        {
+            CreateMap<Company, CompanyViewModel>().ReverseMap();
+        }   
+    }
+}
