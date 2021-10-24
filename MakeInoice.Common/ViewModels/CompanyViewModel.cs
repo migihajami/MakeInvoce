8f43312f548039f0aeb3abcfc9c2fd787ba71e55
Alexey Bubley <alexey.bubley@discovercars.com>
2021-10-24 21:05:53 +0300

    Add addresses ids into Company view model

--- MakeInoice.Common/ViewModels/CompanyViewModel.cs
+++ MakeInoice.Common/ViewModels/CompanyViewModel.cs
@@ -7,12 +7,15 @@
     /// <date>2021-10-18</date>
     public class CompanyViewModel
     {
+        public int CompanyID { get; set; }
         public string LegalName { get; set; }
         public string RegNumber { get; set; }
         public string VatNumber { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public int? LegalAddressID { get; set; }
         public AddressViewModel LegalAdress { get; set; }
+        public int? PostalAddressID { get; set; }
         public AddressViewModel PostalAddress { get; set; }
     }
 }
