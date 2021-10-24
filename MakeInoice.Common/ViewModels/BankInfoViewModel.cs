ea037a866ca8eca5ae08bc6c4b48c79f866a3264
Alexey Bubley <alexey.bubley@discovercars.com>
2021-10-24 22:45:22 +0300

    Add BankInfo view model

---
+++ MakeInoice.Common/ViewModels/BankInfoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeInoice.Common.ViewModels
+{
+    public class BankInfoViewModel
+    {
+        public int BankInfoID { get; set; }
+
+        public string BankName { get; set; }
+
+        public int BankAddressID { get; set; }
+
+        public int? AddressID { get; set; }
+        public AddressViewModel Address { get; set; }
+
+        public string Swift { get; set; }
+
+        public string AccountNumber { get; set; }
+
+    }
+}
