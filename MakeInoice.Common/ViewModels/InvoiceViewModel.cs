add3fc6543130c4e2f36f18b61321c9217b2ec5b
Alexey Bubley <alexey.bubley@discovercars.com>
2021-10-27 08:49:01 +0300

    Add data annotations into InvoiceViewModel

--- MakeInoice.Common/ViewModels/InvoiceViewModel.cs
+++ MakeInoice.Common/ViewModels/InvoiceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MakeInoice.Common.ViewModels
 {
@@ -10,22 +11,31 @@ namespace MakeInoice.Common.ViewModels
     /// <date>2021-10-18</date>
     public class InvoiceViewModel
     {
+        public int InvoiceID { get; set; }
+
+        [Required]
         public string Number { get; set; }
 
+        [Required]
+        [MaxLength(3)]
         public string Currency { get; set; }
 
+        [Required]
         public DateTime InvoiceDate { get; set; }
 
+        [Required]
         public DateTime DueDate { get; set; }
 
         public bool VatApplicable { get; set; }
 
         public int VatPercentage { get; set; }
 
-        public List<InvoiceItemViewModel> InvoiceItems { get; set; }
-
+        [Required]
+        [Range(1, int.MaxValue)]
         public int CompanyID { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue)]
         public int ContractorID { get; set; }
     }
 }
