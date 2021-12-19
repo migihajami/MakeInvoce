79f19c5b4fc4e902adf0c0b5fcc38480d30eb863
Alexey Bubley <alexey.bubley@yandex.ru>
2021-12-19 16:59:43 +0200

    Add frontend

---
+++ MakeInvoice.Frontend/Services/AppConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeInvoice.Frontend.Services
+{
+    public class AppConfiguration
+    {
+        public AppConfiguration(IConfiguration config)
+        {
+            var conf =  config.GetSection("AppConfiguration");
+            ApiUrl = conf["ApiUrl"];
+        }
+
+        public string ApiUrl { get; set; }
+    }
+}
---
+++ MakeInvoice.Frontend/Services/TokenKeeper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeInvoice.Frontend.Services
+{
+    public class TokenKeeper
+    {
+        private string _token;
+
+        private DateTime _ExpiredDate;
+
+        public TokenKeeper()
+        {
+            _ExpiredDate = DateTime.Now.AddSeconds(-1);
+        }
+
+        public TokenKeeper(string token, TimeSpan lifeTime)
+        {
+            RenewToken(token, lifeTime);
+        }
+
+        public bool IsExpired => DateTime.Now > _ExpiredDate;
+
+        public string Token
+        {
+            get
+            {
+                if (IsExpired)
+                    throw new Exception("Token is expired");
+
+                return _token;
+            }
+        }
+
+        public void RenewToken(string token, TimeSpan lifeTime)
+        {
+            _token = token;
+            _ExpiredDate = DateTime.Now.Add(lifeTime);
+
+        }
+    }
+}
